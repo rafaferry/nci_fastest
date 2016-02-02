fcd8b5a56195958e0ed5a9ef1bd143fec40e5823
Rafael Farias <rafaferry@gmail.com>
2016-02-02 12:11:16 -0200

    pequenos ajustes

--- node-image/register-node.sh
+++ node-image/register-node.sh
@@ -3,4 +3,7 @@ xvfb-run --server-args=":99.0 -screen 0 2000x2000x16 -ac" \
 java -jar selenium-server-standalone-${VERSION}.jar \
 -role node \
 -hub http://$HUB_1_PORT_4444_TCP_ADDR:4444/grid/register \
+-browserTimeout 150000 \
+-timeout 150000 \
 -browser browserName=firefox,maxInstances=5
+
