c5f3906ea3e035b6c5555a2a52c4e7951673e3ea
Rafael Farias <rafaferry@gmail.com>
2016-02-02 15:07:41 -0200

    setup with composer-network instead of links

--- start-grid.sh
+++ start-grid.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 'usage: "./start-grid.sh [quantidadeDeNodes]" se a quantidade de nodes nao for passada por parametro, inicia 2 nodes.'
-docker-compose up -d
+docker-compose --x-networking up -d
 
 if [ $# -eq 0 ]
 then
-	docker-compose scale firefox=2
+	docker-compose --x-networking scale firefox=2
 else
-	docker-compose scale firefox=$1
+	docker-compose --x-networking scale firefox=$1
 fi
