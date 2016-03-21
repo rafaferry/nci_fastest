23833c3bd0948cb240de390a6bd9c9415180ef80
Rafael Farias Silva <rafaferry@gmail.com>
2016-03-21 09:24:44 -0300

    ajustes no ambiente

--- jenkins/start_jenkins.sh
+++ jenkins/start_jenkins.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+echo "Starting Jenkins at port 8080"
 nohup java  -XX:MaxPermSize=256m  -Xmx1024M -jar jenkins.war > jenkins_log.txt 2>&1
--- start-grid.sh
+++ start-grid.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 'usage: "./start-grid.sh [quantidadeDeNodes]" se a quantidade de nodes nao for passada por parametro, inicia 2 nodes.'
-docker-compose --x-networking up -d
+/usr/local/bin/docker-compose --x-networking up -d
 
 if [ $# -eq 0 ]
 then
-	docker-compose --x-networking scale firefox=2
+	/usr/local/bin/docker-compose --x-networking scale firefox=2
 else
-	docker-compose --x-networking scale firefox=$1
+	/usr/local/bin/docker-compose --x-networking scale firefox=$1
 fi
